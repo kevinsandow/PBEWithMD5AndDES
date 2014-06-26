5061bf6d8b626b0dc982b715695c8f19e8191569
Danilo Abbasciano <danilo.abbasciano@babel.it>
2014-06-26 15:27:35 +0200

    changes to generate the same encrypted bytes as OpenSSL when using DES with password (hashed) encryption.

--- PbeWithMd5AndDes.php
+++ PbeWithMd5AndDes.php
@@ -2,22 +2,72 @@
 
 class PbeWithMd5AndDes
 {
+   /**
+    * "Magic" keyword used by OpenSSL to put at the beginning of the encrypted
+    * bytes.
+    */
+    private static $MAGIC_SALTED_BYTES = "Salted__";
 
-    public static function encrypt($data, $keystring, $salt, 
-		$iterationsMd5, $segments)
+    private static function _getCharRandonSalt($length = 16)
     {
+        $salt = '';
+	for ($n = 0; $n < $length; $n++)
+	    $salt .= dechex(mt_rand(0, 0xF));
+  
+	return $salt;
+    }
+
+    public static function encrypt($data, $keystring, $salt = null, 
+		$iterationsMd5 = 1, $segments = 1)
+    {
+        $useRandomSalt = false;
+        if ($salt === null)
+	{
+	    $salt = PbeWithMd5AndDes::_getCharRandonSalt();
+	    $useRandomSalt = true;
+	    /**
+	     * Number of iterations -
+	     *  needs to be set to 1 for our roundtrip to work.
+	     */
+	    $iterationsMd5 = 1;
+        }
+
         $pkcsKeyGenerator = new PkcsKeyGenerator(
 			$keystring, $salt, $iterationsMd5, $segments);
 		
         $encryptor = new DesEncryptor(
 			$pkcsKeyGenerator->getKey(), $pkcsKeyGenerator->getIv());
 		
-        return base64_encode($encryptor->transformFinalBlock($data));
+        $crypt = $encryptor->transformFinalBlock($data);
+
+	if ($useRandomSalt) {
+	  // add the magic keyword, salt informazionr and encrypted byte
+	  $crypt =  PbeWithMd5AndDes::$MAGIC_SALTED_BYTES .
+	    hex2bin($salt) .
+	    $crypt;
+	}
+
+	// base64 encode so we can send it around as a string
+	return base64_encode($crypt);
     }
 
-    public static function decrypt($data, $keystring, $salt,
-		$iterationsMd5, $segments)
+    public static function decrypt($data, $keystring, $salt = null,
+		$iterationsMd5 = 1, $segments = 1)
     {
+        if ($salt === null)
+	{
+	    // get the salt information from the input
+	    $salt = bin2hex(substr(base64_decode($data), 8, 8));
+
+	    $data = base64_encode(substr(base64_decode($data), 16));
+
+	    /**
+	     * Number of iterations -
+	     *  needs to be set to 1 for our roundtrip to work.
+	     */
+	    $iterationsMd5 = 1;
+	}
+
         $pkcsKeyGenerator = new PkcsKeyGenerator(
 			$keystring, $salt, $iterationsMd5, $segments);
 
--- README.md
+++ README.md
@@ -4,3 +4,5 @@ PBEWithMD5AndDES
 PHP implementation for passphrase based encryption (PBE) as defined in PKCS#5 version 2.0 (RFC 2898)
 
 The key for the cipher (here DES) is derived from the passphrase by applying a hashfunction (here MD5) several times on it.
+
+You can use to generate the same encrypted bytes as OpenSSL when using DES with password (hashed) encryption.
