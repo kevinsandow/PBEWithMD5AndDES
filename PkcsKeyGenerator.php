613159c2ea0e7d8c978fede077e479648e807ec2
jonloh <cloud3z@gmail.com>
2019-06-20 10:14:08 +0800

    Migrate depreciated mcrypt to openssl.

--- PkcsKeyGenerator.php
+++ PkcsKeyGenerator.php
@@ -1,55 +1,47 @@
 <?php
 
-class PkcsKeyGenerator 
-{
+class PkcsKeyGenerator {
 
     protected $_key;
     protected $_iv;
 
     /**
-	 * @return string
+     * @return string
      */
-    public function getKey()
-    {
-        return $this->_key;
+    public function getKey() {
+      return $this->_key;
     }
 
     /**
      * Gets the initialization vector.
-	 * @return string
+	   * @return string
      */
-    public function getIv()
-    {
-        return $this->_iv;
+    public function getIv() {
+      return $this->_iv;
     }
 
-    function __construct($keystring, $salt, $iterationsMd5, $segments)
-    {
-        $salt = pack('H*', $salt);
-        $keyMaterial = '';
-        $data = $keystring . $salt;
-        $result = '';
+    function __construct($keystring, $salt, $iterationsMd5, $segments) {
+      $salt = pack('H*', $salt);
+      $keyMaterial = '';
+      $data = $keystring . $salt;
+      $result = '';
 
-        for ($j = 0; $j < $segments; $j++)
-        {
-            if ($j == 0)
-            {
-                $result = $data;
-            }
-            else
-            {
-                $result .= $data;
-            }
-
-            for ($i = 0; $i < $iterationsMd5; $i++)
-            {
-                $result = md5($result, true);
-            }
-
-            $keyMaterial .= $result;
+      for ($j = 0; $j < $segments; $j++) {
+        if ($j == 0) {
+          $result = $data;
         }
+        else {
+          $result .= $data;
+        }
+        
+        for ($i = 0; $i < $iterationsMd5; $i++) {
+          $result = md5($result, true);
+        }
+        
+        $keyMaterial .= $result;
+      }
 
-        $this->_key = substr($keyMaterial, 0, 8);
-        $this->_iv = substr($keyMaterial, 8, 8);
+      $this->_key = substr($keyMaterial, 0, 8);
+      $this->_iv = substr($keyMaterial, 8, 8);
     }
 }
